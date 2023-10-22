2df8785237990989bad497f40937d901b2f1839f
Artem <golysheeet@gmail.com>
2023-10-22 08:29:54 +0300

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeContextProvider } from './app/providers/theme';
+import { StoreProvider } from './app/providers/store';
 import { router } from './app/providers/router';
 import '@/shared/config/i18n';
 import '@/app/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeContextProvider>
-      <RouterProvider router={router} />
-    </ThemeContextProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+      <StoreProvider>
+        <ThemeContextProvider>
+          <RouterProvider router={router} />
+        </ThemeContextProvider>
+      </StoreProvider>
+    </React.StrictMode>,
 );

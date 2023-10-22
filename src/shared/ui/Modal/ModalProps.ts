41857ec3a33446463092ad27ca1cd3dfe45e6e85
Artem <golysheeet@gmail.com>
2023-10-22 16:00:12 +0300

    add new props to Modal

--- src/shared/ui/Modal/ModalProps.ts
+++ src/shared/ui/Modal/ModalProps.ts
@@ -2,6 +2,8 @@ import { PropsWithChildren } from 'react';
 
 export interface ModalProps extends PropsWithChildren {
   className?: string;
+  lazy?: boolean;
+  removeFromDOMWhenClosed?: boolean;
   open: boolean;
   onClose: () => void;
 }
